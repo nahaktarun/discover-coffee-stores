9c85fbe179533ea25ddb5ac06938045e21aae461
tarunkumarnahak25 <tarunnaha25@gmail.com>
2022-01-05 22:11:06 +0530

    Added the storereducer

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,31 @@ import '../styles/globals.css';
 
 const StoreContext = createContext();
 
+const ACTION_TYPES = {
+  SET_LAT_LONG: 'SET_LAT_LONG',
+  SET_COFFEE_STORES: 'SET_COFFEE_STORES'
+}
+const storeReducer = (state, action)=>{
+  switch(action.type){
+    case ACTION_TYPES.SET_LAT_LONG: {
+        return {...state, latLong: action.payload.latLong}
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES :{
+        return {...state, coffeeStores: action.payload.coffeeStores}
+    }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 const StoreProvider = ({children}) =>{
   const initialState = {
     latLong: "",
-    coffeStores: [],
+    coffeeStores: [],
   }
+  const [state, dispatch] = useReducer(storeReducer, initialState)
   return (
-    <StoreContext.Provider value = {{state: {initialState}}}>
+    <StoreContext.Provider value = {{state, dispatch}}>
       {children}
     </StoreContext.Provider>
   )
